eca22d3918239f4a22bde1128cb9245dccd20dbc
Axe714 <axearzadon@gmail.com>
2022-07-19 12:36:45 -0700

    Importing files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Professional-Portfolio-2
-Revamping my professional portfolio. 
+
+Revamping my professional portfolio using Tailwind CSS
+
+~ Work in Progress ~
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}", "./src/**/*.{html,js,css}", ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tailwindcss')],
+};
