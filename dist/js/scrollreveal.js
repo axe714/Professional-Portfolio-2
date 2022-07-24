af278d09023f55d92c29678d67b0081d0e3decb4
Axe714 <axearzadon@gmail.com>
2022-07-24 10:12:17 -0700

    Added animations

---
+++ dist/js/scrollreveal.js
@@ -0,0 +1,22 @@
+// navbar
+ScrollReveal().reveal('#navbar-logo', {delay:100})
+ScrollReveal().reveal('#main-navbar', {delay:150})
+
+// hero section
+ScrollReveal().reveal('#hero-title', {delay:200})
+ScrollReveal().reveal('#hero-sub-text', {delay:300})
+ScrollReveal().reveal('#hero-img', {delay:400})
+
+
+//about-me section
+ScrollReveal().reveal('#about-me-name', {delay:100})
+ScrollReveal().reveal('#about-me-text', {delay:300})
+
+//projects section
+ScrollReveal().reveal('#projects-header')
+ScrollReveal().reveal('#project-1', {delay: 100})
+ScrollReveal().reveal('#project-2', {delay: 200})
+ScrollReveal().reveal('#project-3', {delay: 300})
+ScrollReveal().reveal('#project-4', {delay: 400})
+ScrollReveal().reveal('#project-5', {delay: 500})
+ScrollReveal().reveal('#project-6', {delay: 600})
